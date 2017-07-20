4d0159afbe3d984cdbd4c8018dd74dff02c75c60
Jared Trouth <jared.trouth@gmail.com>
2017-07-20 09:33:48 -0500

    Prevent text from overflowing menu cards. Add full description to title property

--- resources/views/menu/index.blade.php
+++ resources/views/menu/index.blade.php
@@ -6,7 +6,7 @@
   <div id="jumbotron" class="jumbotron">
     <div class="menu-board">
       <h1 class="text-center"><u>Today's Menu</u></h1>
-      @if ($menuitems->count())
+      @if (!$menuitems->count())
         <div class="alert alert-danger">
           <p>Error retrieving menu items.</p>
         </div>
@@ -24,47 +24,46 @@
 @section('content')
   <div class="container">
     <h1 class="text-center">Other Fare</h1>
+    @if (!$menuitems->count())
+      <div class="alert alert-danger">
+        <p>Error retrieving menu items.</p>
+      </div>
+    @else
     <p class="lead">We won't be serving these dishes today, but look for them to make an appearance on George's menu in the future.</p>
-    @for ($i=0; $i < $menuitems->count(); $i++)
-      @if ($i % 4 === 0)
-        <div class="row">
-        @endif
 
-        <div class="col-md-3">
+    @foreach ($menuitems as $item)
+      <div class="col-lg-3 col-md-4 col-sm-6">
+        <div class="panel panel-default menu-item-panel" data-slug="{{ $item->slug }}" title="{{ $item->description }}">
+          <div class="panel-heading">
+            <span class="pull-right">${{ $item->price }}</span>
+            <h4 class="panel-title"><a href="{{url('menu', $item->slug)}}">{{ $item->name }}</a></h4>
+          </div>
+          <div class="panel-body">
+            <div class="row">
 
-          <div class="panel panel-default menu-item-panel" data-slug="{{ $menuitems[$i]->slug }}">
-            <div class="panel-heading">
-              <span class="pull-right">${{ $menuitems[$i]->price }}</span>
-              <h4 class="panel-title"><a href="{{url('menu', $menuitems[$i]->slug)}}">{{ $menuitems[$i]->name }}</a></h4>
-            </div>
-            <div class="panel-body">
-              <div class="row">
+              <div class="col-xs-6">
+                <img src="{{ Storage::url($item->image_path) }}" alt="{{ $item->name }}" class="img-responsive">
+              </div>
 
-                <div class="col-xs-4">
-                  <img src="{{ Storage::url($menuitems[$i]->image_path) }}" alt="{{ $menuitems[$i]->name }}" class="img-responsive">
-                </div>
-
-                <div class="col-xs-8">
-                  {{ str_limit($menuitems[$i]->description, 50) }}
-                </div>
-
+              <div class="col-xs-6">
+                <p><small>{{ $item->description }}</small></p>
               </div>
 
             </div>
-          </div>
 
+          </div>
         </div>
-        @if ($i % 4 === 3)
-        </div>
-      @endif
-    @endfor
+
+      </div>
+    @endforeach
+  @endif
   </div>
 @endsection
 
 @push('scripts')
   <script>
-    $( ".menu-item-panel" ).click( function(e) {
-      window.location.href = '/menu/' + $( this ).attr('data-slug');
-    })
+  $( ".menu-item-panel" ).click( function(e) {
+    window.location.href = '/menu/' + $( this ).attr('data-slug');
+  })
   </script>
 @endpush
