6cfb843d3a7145bcff781628838a1a54cd300184
Jared Trouth <jared.trouth@gmail.com>
2017-07-20 08:07:27 -0500

    Update look and function of menu item cards

--- resources/views/menu/index.blade.php
+++ resources/views/menu/index.blade.php
@@ -6,11 +6,17 @@
   <div id="jumbotron" class="jumbotron">
     <div class="menu-board">
       <h1 class="text-center"><u>Today's Menu</u></h1>
-      <ul>
-        @foreach ($menuitems->where('active', '=', true) as $item)
-          <li title="{{ $item->description }}"><a href="{{ url('menu', $item->slug) }}">{{ $item->name }}</a><span class="pull-right">${{ $item->price }}</span></li>
-        @endforeach
-      </ul>
+      @if ($menuitems->count())
+        <div class="alert alert-danger">
+          <p>Error retrieving menu items.</p>
+        </div>
+      @else
+        <ul>
+          @foreach ($menuitems->where('active', '=', true) as $item)
+            <li title="{{ $item->description }}"><a href="{{ url('menu', $item->slug) }}">{{ $item->name }}</a><span class="pull-right">${{ $item->price }}</span></li>
+          @endforeach
+        </ul>
+      @endif
     </div>
   </div>
 @endsection
@@ -22,34 +28,43 @@
     @for ($i=0; $i < $menuitems->count(); $i++)
       @if ($i % 4 === 0)
         <div class="row">
-      @endif
+        @endif
 
-      <div class="col-md-3">
+        <div class="col-md-3">
 
-        <div class="panel panel-default">
-          <div class="panel-heading">
-            <h4 class="panel-title"><a href="{{url('menu', $menuitems[$i]->slug)}}">{{ $menuitems[$i]->name }}</a></h4>
-          </div>
-          <div class="panel-body">
-            <div class="row">
+          <div class="panel panel-default menu-item-panel" data-slug="{{ $menuitems[$i]->slug }}">
+            <div class="panel-heading">
+              <span class="pull-right">${{ $menuitems[$i]->price }}</span>
+              <h4 class="panel-title"><a href="{{url('menu', $menuitems[$i]->slug)}}">{{ $menuitems[$i]->name }}</a></h4>
+            </div>
+            <div class="panel-body">
+              <div class="row">
 
-              <div class="col-xs-4">
-                <img src="{{ Storage::url($menuitems[$i]->image_path) }}" alt="{{ $menuitems[$i]->name }}" class="img-responsive">
-              </div>
+                <div class="col-xs-4">
+                  <img src="{{ Storage::url($menuitems[$i]->image_path) }}" alt="{{ $menuitems[$i]->name }}" class="img-responsive">
+                </div>
 
-              <div class="col-xs-8">
-                {{ str_limit($menuitems[$i]->description, 50) }}
+                <div class="col-xs-8">
+                  {{ str_limit($menuitems[$i]->description, 50) }}
+                </div>
+
               </div>
 
             </div>
-
           </div>
-        </div>
 
-      </div>
-      @if ($i % 4 === 3)
-      </div>
+        </div>
+        @if ($i % 4 === 3)
+        </div>
       @endif
     @endfor
   </div>
 @endsection
+
+@push('scripts')
+  <script>
+    $( ".menu-item-panel" ).click( function(e) {
+      window.location.href = '/menu/' + $( this ).attr('data-slug');
+    })
+  </script>
+@endpush
