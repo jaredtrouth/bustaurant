14db054c68d550ea23d287acad81d6b3053ba88c
Jared Trouth <jared.trouth@gmail.com>
2017-07-29 12:52:55 -0500

    Add create user method

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,42 +10,46 @@ use App\User;
 
 class AdminController extends Controller
 {
-  public function __construct()
-  {
-    $this->middleware('auth');
-  }
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
-  public function index()
-  {
-    $services = Service::where('endtime', '>=', new \DateTime())->get();
-    $menuitems = MenuItem::orderBy('active', 'desc')->get();
-    $posts = Post::paginate(10);
-    $users = User::all();
-    return view('admin.index', compact(['services', 'menuitems', 'posts', 'users']));
-  }
+    public function index()
+    {
+        $services = Service::where('endtime', '>=', new \DateTime())->get();
+        $menuitems = MenuItem::orderBy('active', 'desc')->get();
+        $posts = Post::paginate(10);
+        $users = User::all();
+        return view('admin.index', compact(['services', 'menuitems', 'posts', 'users']));
+    }
 
-  public function createUser(User $user, Request $request)
-  {
-    $this->validate($request, [
+    public function createUser(Request $request)
+    {
+        $this->validate($request, [
       'name' => 'required|string',
       'email' => 'required|string|email|unique:users',
       'password' => 'required|string|min:6|confirmed'
     ]);
 
-    User::create([
+        User::create([
       'name' => $request->name,
       'email' => $request->email,
       'password' => bcrypt($request->password)
     ]);
 
-    return redirect('admin');
-  }
+        return redirect('admin');
+    }
 
-  public function deleteUser(User $user)
-  {
-    $user->destroy();
+    public function editUser(User $user)
+    {
+        return view('admin.editUser', compact(['user']));
+    }
 
-    return redirect('admin');
-  }
+    public function deleteUser(User $user)
+    {
+        $user->delete();
 
+        return redirect('admin');
+    }
 }
